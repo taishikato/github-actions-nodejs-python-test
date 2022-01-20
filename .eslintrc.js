20aa01b267a00daf49fa03f5a2f668168320ccae
taishikato <taishi.k0903@gmail.com>
2022-01-20 15:54:33 +0900

    add js file

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: false,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {},
+}
